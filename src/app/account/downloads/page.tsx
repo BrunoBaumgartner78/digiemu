e440563e25c096d5eee5839185645c5771b72106
Bruno Baumgartner <bruno@brainbloom.ch>
2025-12-14 03:36:23 +0100

    auth

--- src/app/account/downloads/page.tsx
+++ src/app/account/downloads/page.tsx
@@ -1,7 +1,7 @@
 // src/app/account/downloads/page.tsx
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 import Image from "next/image";
